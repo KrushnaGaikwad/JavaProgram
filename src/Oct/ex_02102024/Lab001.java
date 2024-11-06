532965db066d5635c4769a7fac8a9fee797886d1
krushna <krushnagaikwad456@gmail.com>
2024-11-06 18:39:23 +0530

    Practice

---
+++ src/Oct/ex_02102024/Lab001.java
@@ -0,0 +1,16 @@
+package Oct.ex_02102024;
+
+public class Lab001 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+        System.out.println("Krushna Gaikwad");
+    }
+
+
+
+
+
+
+
+}
